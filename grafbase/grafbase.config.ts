71c15f931aef96dad5d53c133b891977910ddaa2
Tarek Meliani <t.meliani@esi-sba.dz>
2023-08-20 20:12:08 +0100

    fix/PascalCase

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,23 +1,23 @@
 import { g, auth, config } from '@grafbase/sdk'
 
 
-const user = g.model("user", {
+const User = g.model("User", {
   name : g.string().length({ min : 4, max: 20}),
   email : g.string().unique(),
   pfp : g.url(),
   bio : g.string(),
   githubUrl : g.string().optional(),
   linkedinUrl : g.string().optional(),
-  projects : g.relation(() => project).list().optional(),
+  projects : g.relation(() => Project).list().optional(),
 })
-const project = g.model("project" , {
+const Project = g.model("Project" , {
   title : g.string().length({ min : 4}),
   desc : g.string(),
   image : g.url(),
   liveUrl : g.url(),
   githubUrl : g.url(),
   category : g.string().search(),
-  author : g.relation(() => user),
+  author : g.relation(() => User),
 })
 
 export default config({
